b1322b04ffe40b43a356c4527e930f71d3aa8dc0
Onur <OnurM@live.com>
2016-02-03 09:05:44 +0100

    Added lab 4 to my WCF_labs repo

--- WCF_Labb_3/NorthwindService/ShipperService.svc.cs
+++ WCF_Labb_3/NorthwindService/ShipperService.svc.cs
@@ -12,6 +12,7 @@ namespace NorthwindService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ShipperService" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select ShipperService.svc or ShipperService.svc.cs at the Solution Explorer and start debugging.
+    //[ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
     public class ShipperService : IShipperService
     {
 
